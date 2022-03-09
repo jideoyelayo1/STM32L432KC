7fde10471dd05e1be0ee049d8b22a6f454cb4800
JideO <JideOyelayo1@Gmail.com>
2022-03-09 21:20:42 +0000

    I am not doing graphs

--- test1/Form2.Designer.cs
+++ test1/Form2.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace test1
+﻿using Syncfusion.UI.Xaml.Charts;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace test1
 {
     partial class TimerWindow
     {
@@ -32,6 +35,14 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
             this.TIM1_textbox = new System.Windows.Forms.TextBox();
+            this.sqlCommand2 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.TIM_1_button = new System.Windows.Forms.Button();
+            this.TIM_2_button = new System.Windows.Forms.Button();
+            this.TIM_3_button = new System.Windows.Forms.Button();
+            this.TIM_6_button = new System.Windows.Forms.Button();
+            this.TIM_7_button = new System.Windows.Forms.Button();
+            this.TIM_15_button = new System.Windows.Forms.Button();
+            this.TIM_16_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -63,17 +74,94 @@
             // 
             // TIM1_textbox
             // 
-            this.TIM1_textbox.Location = new System.Drawing.Point(356, 250);
+            this.TIM1_textbox.Location = new System.Drawing.Point(356, 234);
             this.TIM1_textbox.Multiline = true;
             this.TIM1_textbox.Name = "TIM1_textbox";
-            this.TIM1_textbox.Size = new System.Drawing.Size(74, 188);
+            this.TIM1_textbox.Size = new System.Drawing.Size(74, 204);
             this.TIM1_textbox.TabIndex = 25;
+            // 
+            // sqlCommand2
+            // 
+            this.sqlCommand2.CommandTimeout = 30;
+            this.sqlCommand2.Connection = null;
+            this.sqlCommand2.Notification = null;
+            this.sqlCommand2.Transaction = null;
+            // 
+            // TIM_1_button
+            // 
+            this.TIM_1_button.Location = new System.Drawing.Point(436, 199);
+            this.TIM_1_button.Name = "TIM_1_button";
+            this.TIM_1_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_1_button.TabIndex = 26;
+            this.TIM_1_button.Text = "Timer 1";
+            this.TIM_1_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_2_button
+            // 
+            this.TIM_2_button.Location = new System.Drawing.Point(436, 234);
+            this.TIM_2_button.Name = "TIM_2_button";
+            this.TIM_2_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_2_button.TabIndex = 27;
+            this.TIM_2_button.Text = "Timer 2";
+            this.TIM_2_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_3_button
+            // 
+            this.TIM_3_button.Location = new System.Drawing.Point(436, 269);
+            this.TIM_3_button.Name = "TIM_3_button";
+            this.TIM_3_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_3_button.TabIndex = 28;
+            this.TIM_3_button.Text = "Timer 3";
+            this.TIM_3_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_6_button
+            // 
+            this.TIM_6_button.Location = new System.Drawing.Point(436, 304);
+            this.TIM_6_button.Name = "TIM_6_button";
+            this.TIM_6_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_6_button.TabIndex = 29;
+            this.TIM_6_button.Text = "Timer 6";
+            this.TIM_6_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_7_button
+            // 
+            this.TIM_7_button.Location = new System.Drawing.Point(436, 339);
+            this.TIM_7_button.Name = "TIM_7_button";
+            this.TIM_7_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_7_button.TabIndex = 30;
+            this.TIM_7_button.Text = "Timer 7";
+            this.TIM_7_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_15_button
+            // 
+            this.TIM_15_button.Location = new System.Drawing.Point(436, 374);
+            this.TIM_15_button.Name = "TIM_15_button";
+            this.TIM_15_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_15_button.TabIndex = 31;
+            this.TIM_15_button.Text = "Timer 15";
+            this.TIM_15_button.UseVisualStyleBackColor = true;
+            // 
+            // TIM_16_button
+            // 
+            this.TIM_16_button.Location = new System.Drawing.Point(436, 409);
+            this.TIM_16_button.Name = "TIM_16_button";
+            this.TIM_16_button.Size = new System.Drawing.Size(94, 29);
+            this.TIM_16_button.TabIndex = 32;
+            this.TIM_16_button.Text = "Timer 16";
+            this.TIM_16_button.UseVisualStyleBackColor = true;
             // 
             // TimerWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TIM_16_button);
+            this.Controls.Add(this.TIM_15_button);
+            this.Controls.Add(this.TIM_7_button);
+            this.Controls.Add(this.TIM_6_button);
+            this.Controls.Add(this.TIM_3_button);
+            this.Controls.Add(this.TIM_2_button);
+            this.Controls.Add(this.TIM_1_button);
             this.Controls.Add(this.TIM1_textbox);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.LoadTimers);
@@ -91,5 +179,14 @@
         private DataGridView dataGridView1;
         private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
         private TextBox TIM1_textbox;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand2;
+        private SfChart chartDisplay;
+        private Button TIM_1_button;
+        private Button TIM_2_button;
+        private Button TIM_3_button;
+        private Button TIM_6_button;
+        private Button TIM_7_button;
+        private Button TIM_15_button;
+        private Button TIM_16_button;
     }
 }
--- test1/Form2.cs
+++ test1/Form2.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Syncfusion.UI.Xaml.Charts;
 using System.Windows.Forms.DataVisualization;
 
+
 namespace test1
 {
     public partial class TimerWindow : Form
@@ -19,31 +20,43 @@ namespace test1
             InitializeComponent();
             Startloading_Tim1();
         }
+        SfChart chart = new SfChart() { Header = "Chart" };
+        
 
         private void LoadTimers_Click(object sender, EventArgs e)
         {
-
-        }
-        private void GenerateTimer1()
-        {
-            
-            //var objChart = chart.ChartAreas[0];                    
-            
 
         }
         private async void Startloading_Tim1()
         {
             while (true)
             {
-                this.TIM1_textbox.Text = "TIM1: " + Convert.ToString(STM32.LoadMem(0x40012c10)) + "\n";
+                this.TIM1_textbox.Text = "TIM1: " +    Convert.ToString(STM32.LoadMem(0x40012c10)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM2: " + Convert.ToString(STM32.LoadMem(0x40000010)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM3: " + Convert.ToString(STM32.LoadMem(0x40000410)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM6: " + Convert.ToString(STM32.LoadMem(0x40010c10)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM7: " + Convert.ToString(STM32.LoadMem(0x40014c10)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM15: " + Convert.ToString(STM32.LoadMem(0x40014010)) + "\n";
                 this.TIM1_textbox.Text += "\nTIM16: " + Convert.ToString(STM32.LoadMem(0x40014410)) + "\n";
+
+                if(STM32.LoadMem(0x40012c10) == 1) this.TIM_1_button.BackColor = Color.Green;//TIM1
+                else this.TIM_1_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40000010) == 1) this.TIM_2_button.BackColor = Color.Green;//TIM2
+                else this.TIM_2_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40000410) == 1) this.TIM_3_button.BackColor = Color.Green;//TIM3
+                else this.TIM_3_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40010c10) == 1) this.TIM_6_button.BackColor = Color.Green;//TIM6
+                else this.TIM_6_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40014c10) == 1) this.TIM_7_button.BackColor = Color.Green;//TIM7
+                else this.TIM_7_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40014010) == 1) this.TIM_15_button.BackColor = Color.Green;//TIM15
+                else this.TIM_15_button.BackColor = Color.DarkOrange;
+                if (STM32.LoadMem(0x40014410) == 1) this.TIM_16_button.BackColor = Color.Green;//TIM16
+                else this.TIM_16_button.BackColor = Color.DarkOrange;
+
                 await Task.Delay(10);
             }
         }
+
     }
 }
